451f42794321c533704796a5e6d3fed19adf990b
Brian Moriguchi <brian@beckos.com>
2022-06-28 11:29:14 -0700

    Initial commit

--- VideoStreamingSample/ViewController.swift
+++ VideoStreamingSample/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func playVideo(_ sender: AnyObject) {
+        
+        guard let url = URL(string: "https://firebasestorage.googleapis.com/v0/b/videostreamingsample-e9f45.appspot.com/o/Kinkakuji.mp4?alt=media&token=691fd62e-77e0-4fd4-9a5a-034a43bdac26") else { return }
+        
+    
+    let player = AVPlayer(url: url)
+    let controller = AVPlayerViewController()
+    controller.player = player
+    
+    present(controller, animated: true) {
+        player.play()
     }
-
-
+    
+    }
 }
 
