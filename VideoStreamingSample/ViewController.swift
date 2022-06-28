38a0c5980bdec37923479fef0d83fe5bfb68e1a1
Brian Moriguchi <brian@beckos.com>
2022-06-28 09:07:57 -0700

    Initial Commit

---
+++ VideoStreamingSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoStreamingSample
+//
+//  Created by Tatsuya Moriguchi on 6/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
